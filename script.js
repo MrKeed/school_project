0fd24539ef807ffc7482cb65f9cc7fb29696c424
mr_ked <168069768+MrKeed@users.noreply.github.com>
2025-03-17 10:07:15 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
+document.getElementById('orderForm').addEventListener('submit', function(event) {
+    event.preventDefault(); // Отменяем стандартное поведение формы
 
+    const formData = new FormData(this); // Получаем данные из формы
+    const data = {};
+
+    for (const [key, value] of formData.entries()) {
+        data[key] = value;
+    }
+
+    console.log(data); // Выводим данные в консоль (замените на отправку на сервер)
+});
